d8576a41e1c3513bb7d114f21ee1569172c72184
Nikhil Nanjappa <94963727+NikhilNanjappa@users.noreply.github.com>
2025-09-02 10:49:52 +0100

    added pagination

--- src/views/dashboard/dashboard.js
+++ src/views/dashboard/dashboard.js
@@ -1,10 +1,12 @@
 import { handleServiceResponse } from '../../lib/error-handler';
+import { buildPagination } from '../../lib/pagination';
 
 const get = async (req, res) => {
   try {
     return res.render('dashboard', {
       pageData: {
         message: 'username',
+        // pagination: buildPagination(req, URLS.dashboard.split('?')[0])
       },
     });
   } catch (err) {
