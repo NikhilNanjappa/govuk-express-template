000cea42617f3219039cb3e573ed3d8e1c36cce5
Nikhil Nanjappa <94963727+NikhilNanjappa@users.noreply.github.com>
2025-11-21 16:22:31 +0000

    Update configure-static-files.js

--- src/server/middlewares/configure-static-files.js
+++ src/server/middlewares/configure-static-files.js
@@ -1,14 +1,35 @@
-import express from 'express';
-import favicon from 'serve-favicon';
+import express from "express";
+import favicon from "serve-favicon";
 
 export default (app) => {
-  app.use('/images/favicon.ico', express.static('node_modules/govuk-frontend/govuk/assets/images/favicon.ico'));
-  app.use('/favicon.ico', express.static('node_modules/govuk-frontend/govuk/assets/images/favicon.ico'));
-  app.use(favicon('node_modules/govuk-frontend/govuk/assets/images/favicon.ico'));
-  app.use('/public', express.static('src/public/'));
-  app.use('/assets', express.static('node_modules/govuk-frontend/govuk/assets/'));
-  app.use('/govuk-frontend', express.static('node_modules/govuk-frontend/govuk'));
-  app.use('*/style.css', express.static('dist/style.css'));
-  app.use('/app.js', express.static('dist/main.bundle.js'));
-  app.use('/govuk-frontend/components/', express.static('node_modules/govuk-frontend/govuk/components'));
+  app.use(
+    "/images/favicon.ico",
+    express.static(
+      "node_modules/govuk-frontend/dist/govuk/assets/images/favicon.ico",
+    ),
+  );
+  app.use(
+    "/favicon.ico",
+    express.static(
+      "node_modules/govuk-frontend/dist/govuk/assets/images/favicon.ico",
+    ),
+  );
+  app.use(
+    favicon("node_modules/govuk-frontend/dist/govuk/assets/images/favicon.ico"),
+  );
+  app.use("/public", express.static("src/public/"));
+  app.use(
+    "/assets",
+    express.static("node_modules/govuk-frontend/dist/govuk/assets/"),
+  );
+  app.use(
+    "/govuk-frontend",
+    express.static("node_modules/govuk-frontend/dist/govuk"),
+  );
+  app.use(/.*\/style\.css$/, express.static("dist/style.css"));
+  app.use("/app.js", express.static("dist/main.bundle.js"));
+  app.use(
+    "/govuk-frontend/components/",
+    express.static("node_modules/govuk-frontend/dist/govuk/components"),
+  );
 };
