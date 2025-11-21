5b3e1f0b6f3afdcc19f8ff36edb9fbec1812974f
Nikhil Nanjappa <94963727+NikhilNanjappa@users.noreply.github.com>
2025-11-21 16:19:09 +0000

    Update configure-headers.js

--- src/server/middlewares/configure-headers.js
+++ src/server/middlewares/configure-headers.js
@@ -1,22 +1,22 @@
-import helmet from 'helmet';
+import helmet from "helmet";
 
 const configureHeaders = () => {
   const options = {
     referrerPolicy: {
-      policy: 'no-referrer',
+      policy: "no-referrer",
     },
     contentSecurityPolicy: {
       useDefaults: false,
       directives: {
         defaultSrc: ["'self'"],
-        imgSrc: ["'self'", 'https://*.google-analytics.com/'],
-        connectSrc: ["'self'", 'https://*.google-analytics.com/'],
+        imgSrc: ["'self'", "https://*.google-analytics.com/"],
+        connectSrc: ["'self'", "https://*.google-analytics.com/"],
         scriptSrc: [
           "'self'",
-          "'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='",
+          "'sha256-GUQ5ad8JK5KmEWmROf3LZd9ge94daqNvd8xy9YS1iDw='",
           "'sha256-4knFDLESmwHUzbx7TW2n50F8kKYw3C0C8Atyk8vBsZo='",
-          'https://*.google-analytics.com/',
-          'https://*.googletagmanager.com/',
+          "https://*.google-analytics.com/",
+          "https://*.googletagmanager.com/",
         ],
       },
     },
